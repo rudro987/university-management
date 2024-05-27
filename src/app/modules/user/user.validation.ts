c4ea21fc12015de1ab8a6c16b8eb2a65f459a96c
Tanvir Rudro <rudro987@gmail.com>
2024-05-27 16:26:05 +0600

    user interface, model, route, controller and services setup completed

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const userValidationSchema = z.object({
+    id: z.string(),
+    password: z.string().max(20, {message: 'Password can not be more then 20 characters'}),
+    needsPasswordChange: z.boolean().optional().default(true),
+    roles: z.enum(['student', 'faculty', 'admin']),
+    status: z.enum(['in-progress', 'blocked']).default('in-progress'), 
+    isDeleted: z.boolean().optional().default(false),
+});
+
+export const UserValidation = {
+    userValidationSchema
+};
