dcda4f5f026214c8c636370a926bc9453e69bfd8
Tanvir Rudro <rudro987@gmail.com>
2024-06-05 16:23:55 +0600

    academic semester schema and model created

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -1,9 +1,9 @@
-export type Month = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December'
+export type TMonths = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December'
 
-export type AcademicSemester = {
+export type TAcademicSemester = {
     name: 'Spring' | 'Summer' | 'Fall';
     code: '01'|'02'|'03';
     year: Date;
-    startMonth: Month;
-    endMonth: Month
+    startMonth: TMonths;
+    endMonth: TMonths
 }
