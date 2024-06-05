25843d7c3a2c6034a2773a956d2452577c533c95
Tanvir Rudro <rudro987@gmail.com>
2024-06-05 17:12:06 +0600

    create academic semester route and controller completed

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,19 @@
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import catchAsync from '../../utils/catchAsync';
+
+const createAcademicSemester = catchAsync(async (req, res, next) => {
+//   const { password, student: studentData } = req.body;
+
+//   const result = await UserServices.createStudentIntoDB(password, studentData);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created succesfully',
+    data: result,
+  })
+});
+
+  export const AcademicSemesterControllers = {
+    createAcademicSemester
+  }
