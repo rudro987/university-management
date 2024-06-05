0b8129dbf29215296ce6a808ff4bbfd4974e6ac0
Tanvir Rudro <rudro987@gmail.com>
2024-06-05 16:15:03 +0600

    academic semester interface setup

---
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,9 @@
+export type Month = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December'
+
+export type AcademicSemester = {
+    name: 'Spring' | 'Summer' | 'Fall';
+    code: '01'|'02'|'03';
+    year: Date;
+    startMonth: Month;
+    endMonth: Month
+}
